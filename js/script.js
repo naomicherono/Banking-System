24037bf45568af657ab45fb63f81934e30686f71
RotichKipkoech <kipkoechrottich@gmail.com>
2023-05-25 11:27:28 +0300

    added javascript to ebable validate login

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function attemptLogin() {
+    var inputLoginId = document.getElementsByClassName("loginId")[0].value;
+    var inputPassword = document.getElementsByClassName("password")[0].value;
+
+    // login logic
+    var storedLoginId = "123456789";
+    var storedPassword = "SecureBank";
+
+    var loginResult = document.createElement("p");
+    var loginResultText = "";
+
+    if (inputLoginId === storedLoginId && inputPassword === storedPassword) {
+        loginResultText = "Login successful!";
+        {
+            window.location.href="dashboard.html"; //Redirect to dashboard page
+        }
+        // Grant access or perform 
+    } else {
+        loginResultText = "Login failed. Invalid Login ID or password.";
+    }
+
+    loginResult.textContent = loginResultText;
+    loginResult.className = "login-result";
+    
+    var loginPage = document.getElementsByClassName("loginpage")[0];
+    loginPage.appendChild(loginResult);
+}
+
