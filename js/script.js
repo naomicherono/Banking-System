8f0ee80adf743c70fc4434373ffcaa862da298df
Kamente <justinm48000@gmail.com>
2023-05-25 11:52:52 +0300

    addition of the budget buttons that prompt for amount to be budgetted

--- js/script.js
+++ js/script.js
@@ -10,23 +10,49 @@ function toggleTransactDropdown() {
 
 function sendMoney() {
     var mobileNumber = prompt("Enter the mobile number:");
-    if (mobileNumber !== null) {
-        console.log("Sending money to: " + mobileNumber);
-    } else {
-        console.log("Mobile number not entered");
-    }
+    var amount = prompt("Enter the amount in shs:");
+    alert("Payment Received for acc no: " + mobileNumber);
 }
 
 function payBill() {
     var accountNumber = prompt("Enter the account number:");
-    if (accountNumber !== null) {
-        console.log("Paying bill from account: " + accountNumber);
-    }
+    var amount = prompt("Enter the amount in shs:");
+    alert("Payment Received for acc no: " + accountNumber);
 }
+
 
 function buyGoods() {
     var payBillNumber = prompt("Enter the pay bill number:");
-    if (payBillNumber !== null) {
-        console.log("Buying goods from pay bill: " + payBillNumber);
-    }
+
+}
+
+function handlePaymentReceived() {
+    alert("Payment received");
+}
+
+var budgetData = [];
+function enterAmount(category) {
+  var amount = prompt("Enter the amount in shs for " + category + ":");
+  if (amount !== null) {
+    console.log("Amount for " + category + ": " + amount);
+    var data = { category: category, amount: amount }; 
+    budgetData.push(data); 
+    updateBudgetList(); 
+  } else {
+    console.log("Amount not entered for " + category);
+  }
+  var Amount = confirm("Confirm pesa kwanza ya " + category + " ni: " + amount);
+  
+}
+
+function updateBudgetList() {
+  var budgetList = document.getElementById("budget-list");
+  budgetList.innerHTML = ""; 
+
+  for (var i = 0; i < budgetData.length; i++) {
+    var item = budgetData[i];
+    var listItem = document.createElement("li");
+    listItem.textContent = item.category + ": " + item.amount + " shs";
+    budgetList.appendChild(listItem);
+  }
 }
